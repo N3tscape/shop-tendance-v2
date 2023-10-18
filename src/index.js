bf2129a9870dd954e923994f996513709f704a48
N3tscape <sofiane.debrabant.pro@gmail.com>
2023-10-18 10:05:20 +0200

    Setup redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
+
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./reducers";
+
 import "./index.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
+const store = configureStore({
+  reducer: rootReducer,
+  devTools: true,
+});
+
+const root = document.getElementById("root");
+const appRoot = createRoot(root);
+appRoot.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
