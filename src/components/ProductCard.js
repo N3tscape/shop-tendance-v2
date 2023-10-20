dc9fb9aca981c186d89fe8a852f5730ef9c3021c
N3tscape <sofiane.debrabant.pro@gmail.com>
2023-10-20 14:13:17 +0200

    Stylize product list

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,12 +1,37 @@
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Typography,
+} from "@material-tailwind/react";
+
 export default function ProductCard({ product }) {
   return (
-    <div>
-      <p>{product.title}</p>
-      <p>{product.description}</p>
-      <p>{product.category}</p>
-      <p>{product.basePrice}</p>
-      <p>{product.salePrice}</p>
-      <img src={product.imageUrl} alt={product.title} />
-    </div>
+    <Card className="border rounded-none">
+      <CardHeader
+        shadow={false}
+        floated={false}
+        className="aspect-h-1 aspect-w-1 w-fill h-80 overflow-hidden rounded-none bg-gray-200 lg:aspect-none group-hover:opacity-75"
+      >
+        <img
+          src={product.imageUrl}
+          alt={product.title}
+          className="h-full w-full object-cover object-center"
+        />
+      </CardHeader>
+      <CardBody className="flex justify-between">
+        <div>
+          <Typography variant="h6" color="blue-gray">
+            {product.title}
+          </Typography>
+          <Typography variant="small" color="gray">
+            {product.description}
+          </Typography>
+        </div>
+        <Typography variant="small" color="blue-gray">
+          {product.basePrice.toFixed(2)} €
+        </Typography>
+      </CardBody>
+    </Card>
   );
 }
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import ProductCard from "./ProductCard";
+import { Spinner, Typography } from "@material-tailwind/react";
 
 export default function ProductList() {
   const products = useSelector((state) => state.productReducer);
@@ -13,16 +14,23 @@ export default function ProductList() {
   }, [products]);
 
   return (
-    <div>
-      <h1>Liste des produits</h1>
+    <div className="container mx-auto my-8">
+      <Typography variant="h4" color="blue-gray" className="text-center">
+        Liste des produits
+      </Typography>
       {loading ? (
-        <p>Chargement en cours...</p>
+        <div className="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-blue-gray-700 opacity-50 flex flex-col items-center justify-center">
+          <Spinner className="h-12 w-12 mb-4" />
+          <Typography variant="h3" color="white">
+            Chargement...
+          </Typography>
+        </div>
       ) : (
-        <ul>
+        <div className="mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-10 gap-y-6">
           {products.map((product, index) => (
             <ProductCard key={index} product={product} />
           ))}
-        </ul>
+        </div>
       )}
     </div>
   );
